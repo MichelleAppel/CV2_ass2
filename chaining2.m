b77fd362ba62a4aea911fe9095ff2a4e27d1abcd
Michelle <appel.michelle@gmail.com>
2018-05-18 11:23:40 +0200

    chainz

--- chaining2.m
+++ chaining2.m
@@ -14,20 +14,23 @@ for file_no = 1:length(file_names)
     [ inliers_im1, descriptors, ~, ~ ] = norm_8pt_alg(im1, im2);
 
     if size(descriptors_dictionary, 1) > 0 
-        [ LIA, LOCB ] = ismembertol(double(descriptors).', double(descriptors_dictionary).', 0.05, 'ByRows', true);
+        [ LIA, LOCB ] = ismembertol(double(descriptors).', double(descriptors_dictionary).', 0.6, 'ByRows', true);
        
         % Mathing points
         point_view_matrix(file_no*2-1:file_no*2, LOCB(LIA)) = inliers_im1(:, LIA.');
         point_view_matrix(point_view_matrix == 0) = NaN;
         
-        size(LIA)
+        match_descriptors = descriptors(:, LIA.');
+        match_descriptors_dict = descriptors_dictionary(:, LOCB(LIA));
+        
+%         disp(match_descriptors(1:20, 1:25))
+%         disp(match_descriptors_dict(1:20, 1:25))       
         
         non_matching_descr = descriptors(:, ~LIA.');
         descriptors_dictionary = [ descriptors_dictionary non_matching_descr ];
         
-        % New points
+        % New points in point view matrix
         point_view_matrix(:, end:end+size(~LIA(~LIA==1), 1)) = NaN;
-
         point_view_matrix(file_no*2-1:file_no*2, end-(size(~LIA(~LIA==1), 1))+1:end) = inliers_im1(:, ~LIA.');
 
     else
@@ -35,7 +38,7 @@ for file_no = 1:length(file_names)
         point_view_matrix = inliers_im1;
     end
     
-% point_view_matrix
+plot(point_view_matrix)
 % size(point_view_matrix)
 
 end
